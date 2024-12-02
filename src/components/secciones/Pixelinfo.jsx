313da5426b18b9fab01a1060356503fbc50f2ae4
isma <i00934562@gmail.com>
2024-12-02 00:48:37 -0300

    complicaciones

--- src/components/secciones/Pixelinfo.jsx
+++ src/components/secciones/Pixelinfo.jsx
@@ -6,7 +6,7 @@ export const Pixelinfo=()=>{
             <div className="caja container">
           <div className="nosotros">
             <h1 className=" subtitle">Nosotros</h1><br></br>
-          <p class="roboto-thin-nosotros">Pixel Creatividad Integral es una agencia de diseño gráfico y branding dedicada a potenciar la identidad visual de empresas y marcas. Nos especializamos en la creación de soluciones visuales impactantes que conectan emocionalmente con el público objetivo y reflejan la esencia y los valores de cada <br></br>cliente.</p>
+          <p className="roboto-thin-nosotros">Pixel Creatividad Integral es una agencia de diseño gráfico y branding dedicada a potenciar la identidad visual de empresas y marcas. Nos especializamos en la creación de soluciones visuales impactantes que conectan emocionalmente con el público objetivo y reflejan la esencia y los valores de cada <br></br>cliente.</p>
           </div>
           
              </div>
@@ -17,7 +17,7 @@ export const Pixelinfo=()=>{
             rocket_launch
             </span>
             <p id="title-caja">Creatividad sin límites</p>
-          <p class="roboto-thin ">En Pixel, cada proyecto es abordado con una mente abierta y un enfoque creativo, buscando siempre superar las expectativas y ofrecer resultados únicos y originales.</p>
+          <p className="roboto-thin ">En Pixel, cada proyecto es abordado con una mente abierta y un enfoque creativo, buscando siempre superar las expectativas y ofrecer resultados únicos y originales.</p>
                 </div>
 
                 <div className="boxson">
@@ -33,7 +33,7 @@ export const Pixelinfo=()=>{
             </span>
           <p  id="title-caja">Enfoque centrado en el cliente</p>
   
-         <p  class="roboto-thin ">Nuestro equipo está compuesto por diseñadores y expertos en branding altamente capacitados y apasionados por su trabajo. Nos esforzamos por mantener los más altos estándares de calidad en cada proyecto que emprendemos.</p>
+         <p  className="roboto-thin ">Nuestro equipo está compuesto por diseñadores y expertos en branding altamente capacitados y apasionados por su trabajo. Nos esforzamos por mantener los más altos estándares de calidad en cada proyecto que emprendemos.</p>
 
                 </div>
 
