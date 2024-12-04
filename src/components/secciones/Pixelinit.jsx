eb7993b4a3cbf1028faf97589c74c76251b20b61
isma <i00934562@gmail.com>
2024-12-03 23:25:59 -0300

    Se agregaron animaciones motion

--- src/components/secciones/Pixelinit.jsx
+++ src/components/secciones/Pixelinit.jsx
@@ -1,24 +1,48 @@
 import "../main.css"
+import { motion } from 'framer-motion';
 export const Pixelinit=()=>{
     return(
         <>
         <section className="secctioninicial container-fluid">
      
         <div className="pixel">
+        <motion.h1
+            className="title"
+            initial={{ opacity: 0, y: -50 }}  
+            animate={{ opacity: 1, y: 0 }}    
+            transition={{ duration: 2 }}      
+          >
+            Pixel <br />Creatividad<br /> Integral
+          </motion.h1>
 
-          <h1 className="title">Pixel <br></br>Creatividad<br></br> Integral </h1>
-          <br></br>
-        
-          <p className="parrafo">Transformamos ideas en experiencias visuales <br></br>memorables.</p>
-          <br></br>
-          
-          <button className="estilobutton">Comenzemos</button>
+          <motion.p
+            className="parrafo"
+            initial={{ opacity: 0 }}  
+            animate={{ opacity: 1 }} 
+            transition={{ duration: 2, delay: 0.5 }}  
+          >
+            Transformamos ideas en experiencias visuales <br />memorables.
+          </motion.p>
 
+          <motion.button
+            className="estilobutton"
+            initial={{ scale: 0.8 }} 
+            animate={{ scale: 1 }}    
+            transition={{ duration: 1.5, type: "spring", stiffness: 200 }} 
+          >
+            Comenzemos
+          </motion.button>
         </div>
-        <div className="imgmain">
-          <img className="imgmain" src="/img/hero-banner.png" alt="imgpixel"></img>
 
-        </div>
+        <motion.div
+          className="imgmain"
+          initial={{ opacity: 0, x: 100 }} 
+          animate={{ opacity: 1, x: 0 }}   
+          transition={{ duration: 2 }}      
+        >
+          <img className="imgmain" src="/img/hero-banner.png" alt="imgpixel" />
+        </motion.div>
+   
         
     
 
