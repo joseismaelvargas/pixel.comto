457783572e4005dcc020b22d4aba9c3d592bbf4c
ismael <joseismaelvargas@users.noreply.github.com>
2024-12-10 14:01:32 -0300

    d

--- src/components/page/Contactos.jsx
+++ src/components/page/Contactos.jsx
@@ -1,13 +1,17 @@
 import "./style/contacto.css"
 import { motion } from "framer-motion"
+
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+
+
 export const Contactos=()=>{
+ 
     const form = useRef();
 
     const sendEmail = (e) => {
       e.preventDefault();
-       
+  
       emailjs
         .sendForm('service_0hiib5a', 'template_ixeaf0s', form.current, {
           publicKey: 'YsSHDf_TbSF18GkGC',
@@ -17,13 +21,12 @@ export const Contactos=()=>{
             Swal.fire({
                 position: "top-center",
                 icon: "success",
-                title: "Email Enviado ",
+                title: "Email Enviado",
                 showConfirmButton: false,
-                timer: 1500 
-              
+                timer: 1500
               });
-             console.log('SUCCESS!');
-             form.current.reset()
+              form.current.reset()
+            console.log('SUCCESS!');
           },
           (error) => {
             console.log('FAILED...', error.text);
@@ -31,22 +34,21 @@ export const Contactos=()=>{
         );
     };
   
+  
+ 
     return(
         <>
        <motion.section initial={{ opacity: 0, x: 100 }} 
           animate={{ opacity: 1, x: 0 }}   
           transition={{ duration: 2 }} className="form">
        <div>
-        <h1 style={{fontSize:"40px",
+        <h1 style={{fontSize:"50px",
         color: "rgb(14, 14, 75)"}}>Escalamos Tu,<br></br>marca juntos</h1>
     <img src="img/contact.png" alt="imgcontacto" className="imgcontacto"></img>
-    </div><h1 style={{fontSize:"30px",
-        color: "rgb(14, 14, 75)",
-        margin:"30px"}}>Envianos un Gmail </h1>
-
-    <form ref={form}class="formthend" onSubmit={sendEmail}>
+    </div>
+    <form class="formthend" onSubmit={sendEmail} ref={form} >
   
-       <label for="">Nombre</label>
+       <label for="" >Nombre</label>
        <input type="text" placeholder="Ej:Edward" name="user_name"></input>
        
        <label for="">Correo Electronico*</label>
